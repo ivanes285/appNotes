881e185341e909e8821ba9c321db6d64ae67c8ab
ivan lescano <ivanes285@gmail.com>
2020-09-04 20:02:37 -0500

    Firts Commit

---
+++ main.js
@@ -0,0 +1,62 @@
+document.getElementById("formTask").addEventListener("submit", saveTask);
+
+function saveTask(e) {
+  let title = document.getElementById("title").value;
+  let description = document.getElementById("description").value;
+
+  const task = {
+    title,
+    description,
+  };
+
+  if (localStorage.getItem("tasks") === null) {
+    let tasks = [];
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  } else {
+    let tasks = JSON.parse(localStorage.getItem("tasks"));
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+  getTask();
+  e.preventDefault();
+}
+
+function getTask() {
+  let tasks = JSON.parse(localStorage.getItem("tasks"));
+  let tasksview = document.getElementById("tasks");
+
+  tasksview.innerHTML = ""; //primero vamos a dejarlo limpio
+
+  for (let i = 0; i < tasks.length; i++) {
+    let title = tasks[i].title;
+    let description = tasks[i].description;
+    tasksview.innerHTML += `
+   <div class="card mb-3">
+   <div class="card-body">
+   <p>${title}-${description}</p>
+
+   <a class="btn btn-danger" onclick="deleteTask('${title}')">
+   Delete
+     </a>
+   </div>
+   </div>`;
+  }
+}
+
+function deleteTask(title) {
+   let tasks = JSON.parse(localStorage.getItem("tasks"));
+   for(let i=0; i<tasks.length; i++) {
+    if(tasks[i].title == title) {
+       task.splice(i,1);
+
+    }
+
+   } 
+   localStorage.setItem('tasks', JSON.stringify(tasks));
+
+   getTask(); 
+
+}
+
+getTask();
